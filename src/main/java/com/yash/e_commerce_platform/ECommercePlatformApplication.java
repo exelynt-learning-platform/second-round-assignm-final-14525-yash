6e771fcd6ff7f2d5c00785e1fc0710d5a78e3001
Yash Chougale <yashachougale2001@gmail.com>
2026-03-28 17:45:56 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/yash/e_commerce_platform/ECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yash.e_commerce_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePlatformApplication.class, args);
+	}
+
+}
